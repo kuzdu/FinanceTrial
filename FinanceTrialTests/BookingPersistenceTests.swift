ad898e1df9de89e49a755a21cd22ddcb34f37b27
kuzdu <rothkegel.michael@gmail.com>
2020-08-19 13:32:03 +0200

    add tests for add booking

--- FinanceTrialTests/BookingPersistenceTests.swift
+++ FinanceTrialTests/BookingPersistenceTests.swift
@@ -12,12 +12,13 @@ import XCTest
 
 class BookingPersistenceTests: XCTestCase {
     
-    override func setUpWithError() throws {
+    override func setUp() {
+        super.setUp()
         UserDefaults.standard.removeObject(forKey: "userKey")
     }
     
-    override func tearDownWithError() throws {
-        
+    override func tearDown() {
+        super.tearDown()
     }
     
     func testSavingUser() throws {
@@ -42,6 +43,69 @@ class BookingPersistenceTests: XCTestCase {
             XCTAssertEqual(4, loadedUser.filterBooking(for: .bankAccount).count)
             XCTAssertEqual(6, loadedUser.filterBooking(for: .cash).count)
             XCTAssertEqual(8, loadedUser.filterBooking(for: .creditCard).count)
+            XCTAssertEqual("Michael", loadedUser.name)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+    
+    func testAddBookingToUserWithBookings() throws {
+        // GIVEN a user with some bookings
+        let accountTypes = [AccountType.bankAccount: 1, AccountType.cash: 2, AccountType.creditCard: 0]
+        let bookings = EntityHelper.createBookings(accountTypes: accountTypes)
+        let user = EntityHelper.createUser(bookings: bookings)
+        let bookingPersistence = BookingPersistence()
+        try bookingPersistence.saveUser(user)
+        
+        // WHEN they are added, the should exist add the user
+        do {
+            let newBooking = Booking(account: .cash, expenseCategory: .entertainment, amount: 20.0, date: Date())
+            try bookingPersistence.addBooking(newBooking)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+        
+        
+        // THEN they should exist
+        do {
+            let loadedUser = try bookingPersistence.loadUser()
+            XCTAssertEqual(4, loadedUser.bookings.count)
+            XCTAssertEqual(1, loadedUser.filterBooking(for: .bankAccount).count)
+            XCTAssertEqual(3, loadedUser.filterBooking(for: .cash).count)
+            XCTAssertEqual(0, loadedUser.filterBooking(for: .creditCard).count)
+            XCTAssertEqual("Michael", loadedUser.name)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+    
+    
+    func testAddBookingToUserWithoutBookings() throws {
+        // GIVEN is no user - it is the first start
+        let bookingPersistence = BookingPersistence()
+        
+        // WHEN a new booking is added there should be a user
+        do {
+            let newBooking = Booking(account: .cash, expenseCategory: .entertainment, amount: 20.0, date: Date())
+            try bookingPersistence.addBooking(newBooking)
+
+            let newBooking2 = Booking(account: .cash, expenseCategory: .entertainment, amount: 20.0, date: Date())
+            try bookingPersistence.addBooking(newBooking2)
+
+            let newBooking3 = Booking(account: .creditCard, expenseCategory: .entertainment, amount: 20.0, date: Date())
+            try bookingPersistence.addBooking(newBooking3)
+
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+        
+        // THEN they should exist
+        do {
+            let loadedUser = try bookingPersistence.loadUser()
+            XCTAssertEqual(3, loadedUser.bookings.count)
+            XCTAssertEqual(0, loadedUser.filterBooking(for: .bankAccount).count)
+            XCTAssertEqual(2, loadedUser.filterBooking(for: .cash).count)
+            XCTAssertEqual(1, loadedUser.filterBooking(for: .creditCard).count)
             XCTAssertEqual("Michael", loadedUser.name)
         } catch {
             XCTFail(error.localizedDescription)
